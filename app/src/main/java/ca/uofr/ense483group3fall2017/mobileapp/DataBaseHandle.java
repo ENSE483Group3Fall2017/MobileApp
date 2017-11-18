3b1c418ec97275865081f49511a3ee43c42e5b5c
ahmed35m <muhammadmanzoor16@gmail.com>
2017-11-18 16:31:50 -0600

    updated Db file

--- app/src/main/java/ca/uofr/ense483group3fall2017/mobileapp/DataBaseHandle.java
+++ app/src/main/java/ca/uofr/ense483group3fall2017/mobileapp/DataBaseHandle.java
@@ -9,19 +9,33 @@ import andriod.database.sqlite.SQLiteOpenHelper;
 
 
 public class DatabaseHelper extends SQLiteOpenHelper{
-    public DatabaseHelper (Context context, String name, SQLiteDatabse.CursorFactory factory, int version){
-        super (context , name,factory , version);
+
+    // Name of Data base
+    public static final String DATABASE_NAME = "local_device_db";
+    // Name of Table
+    public static final String TABLE_NAME = "search_result";
+    // Name of Columns
+    public static final String COL1_NAME = "entry_id";
+    public static final String COL2_NAME = "device_found";
+    public static final String COL3_NAME = "distance";
+    public static final String COL4_NAME = "DateTime";
+    public static final String COL5_NAME = "location";
+
+    public DatabaseHelper (Context context){
+        super (context , DATABASE_NAME,null , 1);
     }
 
     @Overide
     public void OnCreate(SQLiteDatabase db)
     {
-
+        string query =" ";
+        query = "CREATE TABLE " +TABLE_NAME+ " (entry_id INTEGER PRIMARY KEY AUTOINCREMENT ,device_found VARCHAR(255) distance FLOAT, DateTime DATETIME ,location TEXT)"+
+    db.execSQL();
     }
 
     public void onUpgrade (SQLiteDatabase db, int oldVer, int newVer)
     {
-        
+
     }
 
 
